d20ac71e2eb652c10baff545cdf06f261273551f
Asif Kamal <asif.kamal1489@gmail.com>
2018-05-23 10:47:11 -0400

    added mouse over message to the class project-details

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+console.log("Welcome to my portfolio site!")
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
