5acf1cbd0d5e087fa8b15da53d4ee81db87233b5
Asif Kamal <asif.kamal1489@gmail.com>
2018-05-24 17:28:11 -0400

    fixed indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 console.log('Welcome to my portfolio site!')
-$(document).ready(function() {
+$(document).ready(function () {
   $('.project').on('mouseenter', function () {
-  $(this).find('.project-details').fadeIn();
-    });
+    $(this).find('.project-details').fadeIn();
+  });
   $('.project').on('mouseleave', function () {
     $(this).find('.project-details').fadeOut();
   })
